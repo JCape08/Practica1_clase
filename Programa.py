d8529f07ec203f51e21f465e9afb0b0da0c358e1
Jose Miguel Duran Capetillo <jose.ariel251200@gmail.com>
2026-04-15 14:43:39 -0600

    se crearon los init

---
+++ Programa.py
@@ -0,0 +1,7 @@
+class Carro:
+    def __init__(self, marca, modelo, color, kilometraje, gasolina):
+        self.__marca = marca
+        self.__modelo = modelo
+        self.__color = color 
+        self.__kilometraje = kilometraje
+        self.__gasolina = gasolina
